d51d1c7b4a4cabad96f0924d4f97f0924958bdc6
cristiansierra-E <cristian.sierra@uabc.edu.mx>
2026-02-26 19:19:34 -0800

    nuevo metodo

--- calculadora.py
+++ calculadora.py
@@ -2,5 +2,10 @@ class Calculadora:
  def add(self, a, b):
     return a + b #hola
 
+
+
+ def multiplicacion(self, a, b):
+    return a * b #hola
+
 #python -m pytest
 #python -m pytest -v
--- test_calculadora.py
+++ test_calculadora.py
@@ -2,3 +2,7 @@ from  calculadora import Calculadora
 def test_add():
     calc = Calculadora()
     assert calc.add(2, 3) == 5
+    
+    def test_multiplicacion(self):
+        calc = Calculadora()
+        assert calc.multiplicacion(2, 3) == 6
